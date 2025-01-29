71478e32f13f7e4f85900d5cd22f304665450ba2
ranjit1024 <ranjitdas2048@gmail.com>
2025-01-29 21:02:02 +0530

    adding new file

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,15 @@ import { Hono } from 'hono'
 
 const app = new Hono()
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+
+
+app.get('/signin', async (c) => {
+  const body = await c.req.json();
+  console.log(body);
+
+  console.log(c.req.header('Authorization'));
+  console.log(c.req.query("param"))
+  return c.text("you are sign in ")
+});
 
 export default app
