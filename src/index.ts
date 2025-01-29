dbad5cc09fceebf8e504f4fe32b8dd30f8b25d89
ranjit1024 <ranjitdas2048@gmail.com>
2025-01-29 21:50:23 +0530

    adding some html file

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,34 @@ import { Hono } from 'hono'
 const app = new Hono()
 //creating middleware
 app.get('/', async (c: any) => {
-  return c.text('Running...')
+  const html: any = `<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<style>
+    body {
+        width: 100%;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+
+    h1 {
+        color: green;
+        font-size: 14vh;
+        text-shadow: 1px 1px 10px rgb(112, 162, 156);
+    }
+</style>
+
+<body>
+    <h1>Running...</h1>
+</body>
+
+</html>`
+  return c.html(html);
 })
 
 async function authMiddleware(c: any, next: any) {
@@ -13,6 +40,7 @@ async function authMiddleware(c: any, next: any) {
     c.text("You dont have access")
   }
 }
+
 app.get('/signin', authMiddleware, async (c) => {
   const body = await c.req.json();
   console.log(body);
